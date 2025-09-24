a58d5403226c594113da65d41d4b3d58e47aa408
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-24 07:16:11 +0300

    Parsed config file

---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Adress string
+}
+
+// We'll give structs tags to the entities of struct to extract contents from yaml file
+
+type Config struct {
+	Env string `yaml:"env" env:"dev" env-required:"true"`
+
+	StoragePath string `yaml:"storage_path" env:"storage.db" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flags := flag.String("config", "", "Path to config file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("config path is not set")
+		}
+	}
+	// setting the config path checker if file doesn't exist the program will stop right away trying it with file handling concept
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("failed to read config file: %s", err.Error())
+	}
+	return &cfg
+}
