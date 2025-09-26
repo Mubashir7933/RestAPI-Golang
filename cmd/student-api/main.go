e58af48109616561ffdfc6e0323527fa55350d4a
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-26 16:13:27 +0300

    setup the server

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Mubashir7933/RestAPI-Golang/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//load config file
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Students API"))
+	})
+	// setup Server
+
+	server := http.Server{
+		Addr:    cfg.Adress,
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server: ", err.Error())
+	}
+	fmt.Println("Server started on port", cfg.Adress)
 }
