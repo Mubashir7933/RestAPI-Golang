fe0e21724a23ec36b5c349f089b09c2e4ae7fec7
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-29 07:28:51 +0300

    Basic setup of API Request and EOF Error handling

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,9 +1,30 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/Mubashir7933/RestAPI-Golang/internal/types"
+	"github.com/Mubashir7933/RestAPI-Golang/internal/utils/response"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to Students API"))
+		var student types.Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if errors.Is(err, io.EOF) {
+			response.WriteJSON(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		slog.Info("Creating a student")
+		response.WriteJSON(w, http.StatusCreated, map[string]string{
+			"message": "student created successfully",
+		})
 	}
 }
