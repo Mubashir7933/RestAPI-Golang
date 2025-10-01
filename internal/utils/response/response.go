150c1263fe04afb0a27cccfe2ff71522aa9a3966
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-10-01 11:06:41 +0300

    Error handled gracefully

--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -5,8 +5,25 @@ import (
 	"net/http"
 )
 
+type Response struct {
+	Status string `json:"status"`          // showing in postman
+	Error  string `json:"error,omitempty"` //omitempty means if there is no error it won't show anything
+}
+
+const (
+	StatusOK    = "OK"
+	StatusError = "error"
+)
+
 func WriteJSON(w http.ResponseWriter, status int, data interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
+}
+
+func GeneralError(err error) Response {
+	return Response{
+		Status: StatusError,
+		Error:  err.Error(),
+	}
 }
